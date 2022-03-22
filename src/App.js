f20105d03288e40dfb97025578833880d3603b08
pawanyogi <yogipawanofficial01@gmail.com>
2022-03-22 19:56:04 +0530

    NavBar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div> This is pawan kumar yogi - A new Developer !!</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <main>
+          <h1>This is pawan kumar yogi - A new Developer !;</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
